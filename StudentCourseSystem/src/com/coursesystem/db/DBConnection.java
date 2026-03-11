2b2bd07bc04a4345d9d0023bfe2eea4a689c0db2
Sweety Paul <sweetypaul2005@gmail.com>
2026-03-11 11:19:28 +0530

    Update DBConnection.java

--- StudentCourseSystem/src/com/coursesystem/db/DBConnection.java
+++ StudentCourseSystem/src/com/coursesystem/db/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/course_allotment";
     private static final String USER = "root";
-    private static final String PASSWORD = "20052005"; // Change this
+    private static final String PASSWORD = ""; // Change this
 
     public static Connection getConnection() throws SQLException {
         try {
